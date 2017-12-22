5a13ca9c872abe7d267ac72e4a74899a898c5556
Levinkov Gleb <bigman212@yandex.ru>
2017-12-22 21:38:32 +1000

    finished docs

--- habrparser/habrparser.py
+++ habrparser/habrparser.py
@@ -8,10 +8,33 @@ def _is_habrahabr(link: str) -> bool:
 
 
 class HabrParser:
+    """
+    The HabrParser object is used to get all the necessary information from the Habrahabr article
+    such as : article title, article author, article groups (hashtags), creation date.
+    Needed information is kept in variables below. All these variables are getting their values
+    as a return result from private methods.
+
+    ::file:: re.py is used for parsing
+
+    Constructor has only one parameter - link to the Habrahabr article. This link is being checked, and
+    if it doesn't go to the Habrahabr site - it writes mistake and exits.
+    For usage - you just need to create :class:'HabrParser' instance and pass the link like this::
+
+           from HabrParser import HabrParser
+           parser = HabrParser('your_link_here')
+
+    After that, you can get information about the article like this::
+
+           article_title = parser.title
+           creation_date = parser.date
+           ...
+
+       """
     def __init__(self, link: str):
         if _is_habrahabr(link):
             self.html_doc = self.__get_html(link)
 
+            self.link = link
             self.title = self.__parse_title()
             self.author = self.__parse_author()
             self.groups = self.__parse_groups()
@@ -23,30 +46,67 @@ class HabrParser:
             exit(1)
 
     def __get_html(self, link):
+        """
+        Gets a raw string html copy of web-page.
+        Uses :file:'urllib.request' to get web-page content.
+        :param link: link to the web-page
+        :return: str representation of html page
+         """
         try:
-            html_page = str(urlopen(link).read().decode('utf8'))
+            html_page = str(urlopen(link).read().decode('utf-8'))
             return html_page
-        except (urllib.error) as e:
-            print(e)
+        except urllib.error.HTTPError as e:
+            print("Something with HTTP: ", e)
+            input()
+            exit(1)
+        except urllib.error.URLError as e:
+            print("Something with URL: ", e)
             input()
             exit(1)
 
     def __parse_title(self) -> str:
+        """
+        Parses the title of habrahabr article with the :file:'re.py' and :meth: findall. Example::
+        Gets the first result of the :meth:'findall' query.
+
+            example_title = "<span class="post__title_text">Retrofit 2 with Android</span>"
+            title = re.findall(:var:'title_regex') # returns ('Retrofit 2 with Android')
+
+        :return: title of the article
+        """
         title_regex = '<span class="post__title-text">(.+?)</span>'
         title = re.findall(title_regex, self.html_doc)
         return title[0]
 
     def __parse_author(self) -> str:
+        """
+        Works same as :meth:'__parse_title', but searches with the different regexp.
+        :return: author of the article
+        """
         author_regex = '<span class="user-info__nickname user-info__nickname_small">(.+?)</span>'
         author = re.findall(author_regex, self.html_doc)
         return author[0]
 
     def __parse_groups(self) -> list:
+        """
+        Parses the groups/hashtags that characterize article. Example::
+            tag_1 = '<a *attributes* class="inline-list__item-link hub-link " *attributes*>Java</a>'
+            tag_2 = '<a *attributes* class="inline-list__item-link hub-link " *attributes*>Kotlin</a>'
+            groups = re.findall(:var:'groups_regex') # returns ('Java', 'Kotlin')
+        :return: list of all groups
+        """
         groups_regex = '<a .*? class="inline-list__item-link hub-link ".*?>(.+?)</a>'
         groups = re.findall(groups_regex, self.html_doc)
         return groups
 
     def __parse_date(self) -> str:
+        """
+        Searches article creation date and converts it to the format 'YYYY-MM-DD'
+        At first gets date in format like 'day month_name year (optional)' After spliting the string into a list
+        and checking each element - returns formatted string.
+        If the second element of the list is char 'в' it means that article creation year = current year = 2017
+        :return: formatted str date in 'YYY-MM-DD'
+        """
         date_regex = '<span class="post__time">(.+?)</span>'
         date = re.findall(date_regex, self.html_doc)
         date = date[0].strip().split(' ')
@@ -66,23 +126,10 @@ class HabrParser:
                 month = "0{}".format(all_months.index(month) + 1)
         return "{}-{}-{}".format(year, month, day)
 
-# TODO: добавить добавление статей
-# def insertion():
-#
-#     # db.get_articles_by_group('Графический дизайн', '2016-01-01', '2018-01-01')
-#     db.get_top_groups()
-#     db.get_top_users()
-#     db.get_articles_by_week()
-#     current_link = "https://habrahabr.ru/company/dataline/blog/336828/"
-#     habr_parser = HabrParser(current_link)
-#
-#     for group in habr_parser.groups:
-#         db.insert_group(Group(group))
-#
-#     db.insert_author(Author(habr_parser.author))
-#     db.insert_article(Article(current_link, habr_parser.title, habr_parser.author, habr_parser.date))
-#
-#     for group in habr_parser.groups:
-#         db.insert_article_data(Article_Group(habr_parser.title, group))
-#
-#     db.commit()
+    # def get_values_dict(self) -> dict:
+    #     values_dict = {}
+    #     values_dict["link"] = self.link
+    #     values_dict["author"] = self.author
+    #     values_dict["groups"] = self.groups
+    #     values_dict["creation_date"] = self.date
+    #     return values_dict
